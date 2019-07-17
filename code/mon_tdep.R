0c049f0fcabe6f99affd610b8f695afa96d8bff0
Jeremy Ash <jeremyd.ash@gmail.com>
2019-07-17 11:27:40 -0500

    paneled deposition figures

--- code/mon_tdep.R
+++ code/mon_tdep.R
@@ -20,6 +20,8 @@ library(extrafont)
 library(gtable)
 library(grid)
 library(ggspatial)
+library(gridExtra)
+library(ggpubr)
 #----------------------------------------------------------------------------
 
 # 
@@ -108,7 +110,7 @@ mon_buffer <- spTransform(mon_buffer, crs_new)
 mon_buffer_sf <- sf::st_as_sf(mon_buffer)
 
 #############################################################################
-## overlay wayne and extract tdep
+## overlay mon and extract tdep
 #############################################################################
 
 
@@ -129,7 +131,170 @@ crop_and_summ(mon_buffer, s_01)
 crop_and_summ(mon_buffer, s_16)
 
 
+#############################################################################
+## crop and plot with grids
+#############################################################################
 
+crop_raster <- function(SHAPEFILE, RASTER) {
+  # cr <- crop(RASTER, extent(SHAPEFILE), snap="out")                    
+  # fr <- rasterize(SHAPEFILE, cr)   
+  # lr <- mask(x=cr, mask=fr)  
+  # val <- getValues(lr)
+  spdf <- as(RASTER, "SpatialPixelsDataFrame")
+  df <- as.data.frame(spdf)
+  colnames(df) <- c("value", "x", "y")
+  df <- df %>% 
+    filter(between(x, 1270000, 1490000)) %>% 
+    filter(between(y, 1720000, 1980000))
+  # lr_df <- broom::tidy(lr)
+}
+
+
+# total n in buffer
+mon_n_01 <- crop_raster(mon_buffer, n_01)
+colnames(mon_n_01)[1] <- "n_01"
+mon_n_16 <- crop_raster(mon_buffer, n_16)
+colnames(mon_n_16)[1] <- "n_16"
+mon_n <- left_join(mon_n_01, mon_n_16) 
+
+
+
+# total s in buffer
+mon_s_01 <- crop_raster(mon_buffer, s_01)
+colnames(mon_s_01)[1] <- "s_01"
+mon_s_16 <- crop_raster(mon_buffer, s_16)
+colnames(mon_s_16)[1] <- "s_16"
+mon_s <- left_join(mon_s_01, mon_s_16) 
+
+
+##-------------
+## nitrogen plotting
+##-------------
+
+# 2001
+summary(mon_n)
+
+n_dep_plot_01 <- ggplot() +
+  geom_tile(aes(x,y, fill = n_01), color = NA, data = mon_n) +
+  theme_minimal() +
+  scale_fill_viridis(direction = 1, name = "Total N \n(kg-N/ha)", limits = c(5,18)) + 
+  geom_sf(fill = NA, color = "white", data = mon_sf) +
+  labs(title = "2000-2002") +
+  theme(axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        panel.spacing = unit(0, "lines"),
+        # plot.margin = margin(0,0,0,0),
+        plot.background = element_blank(),
+        plot.title = element_text(hjust = 0.5, vjust = -5, size = 14),
+        plot.margin = margin(0,2,0,0),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 14),
+        legend.key.height = unit(0.5, "in")) 
+
+# 2016
+n_dep_plot_16 <- ggplot() +
+  geom_tile(aes(x,y, fill = n_16), color = NA, data = mon_n) +
+  theme_minimal() +
+  scale_fill_viridis(direction = 1, name = "Total N \n(kg-N/ha)", limits = c(5,14)) + 
+  geom_sf(fill = NA, color = "white", data = mon_sf) +
+  labs(title = "2015-2017") +
+  theme(axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        panel.spacing = unit(0, "lines"),
+        # plot.margin = margin(0,0,0,0),
+        plot.background = element_blank(),
+        plot.title = element_text(hjust = 0.5, vjust = -5, size = 14),
+        plot.margin = margin(0,2,0,0),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 14),
+        legend.key.height = unit(0.5, "in")) 
+
+n_figs <- grid.arrange(n_dep_plot_01, n_dep_plot_16, nrow = 1)
+n_figs <- annotate_figure(n_figs,
+                fig.lab = "Nitrogen Deposition across Monongahela NF",
+                fig.lab.size = 20,
+                fig.lab.pos = "top.left")
+
+pdf("figures/n_dep_mon_nf.pdf", height = 6, width = 11)
+n_figs
+dev.off()
+
+##-------------
+## sulfur plotting
+##-------------
+
+# 2001
+summary(mon_s)
+
+s_dep_plot_01 <- ggplot() +
+  geom_tile(aes(x,y, fill = s_01), color = NA, data = mon_s) +
+  theme_minimal() +
+  scale_fill_viridis(direction = 1, name = "Total S \n(kg-S/ha)", limits = c(8,45)) + 
+  geom_sf(fill = NA, color = "white", data = mon_sf) +
+  labs(title = "2000-2002") +
+  theme(axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        panel.spacing = unit(0, "lines"),
+        # plot.margin = margin(0,0,0,0),
+        plot.background = element_blank(),
+        plot.title = element_text(hjust = 0.5, vjust = -5, size = 14),
+        plot.margin = margin(0,2,0,0),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 14),
+        legend.key.height = unit(0.5, "in")) 
+
+# 2016
+s_dep_plot_16 <- ggplot() +
+  geom_tile(aes(x,y, fill = s_16), color = NA, data = mon_s) +
+  theme_minimal() +
+  scale_fill_viridis(direction = 1, name = "Total S \n(kg-S/ha)", limits = c(1,8)) + 
+  geom_sf(fill = NA, color = "white", data = mon_sf) +
+  labs(title = "2015-2017") +
+  theme(axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        panel.spacing = unit(0, "lines"),
+        # plot.margin = margin(0,0,0,0),
+        plot.background = element_blank(),
+        plot.title = element_text(hjust = 0.5, vjust = -5, size = 14),
+        plot.margin = margin(0,2,0,0),
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 14),
+        legend.key.height = unit(0.5, "in")) 
+
+s_figs <- grid.arrange(s_dep_plot_01, s_dep_plot_16, nrow = 1)
+s_figs <- annotate_figure(s_figs,
+                          fig.lab = "Sulfur Deposition across Monongahela NF",
+                          fig.lab.size = 20,
+                          fig.lab.pos = "top.left")
+
+pdf("figures/s_dep_mon_nf.pdf", height = 6, width = 11)
+s_figs
+dev.off()
+
+
+#############################################################################
+## crop and plot with facets
+#############################################################################
 
 crop_raster <- function(SHAPEFILE, RASTER) {
   # cr <- crop(RASTER, extent(SHAPEFILE), snap="out")                    
@@ -154,6 +319,9 @@ colnames(mon_n_16)[1] <- "n_16"
 mon_n <- left_join(mon_n_01, mon_n_16) %>% 
   gather(years, n_dep, c("n_01", "n_16"))
 
+n_years_labs <- c("2000-2002", "2015-2017")
+names(n_years_labs) <- c("n_01", "n_16") 
+
 # total s in buffer
 mon_s_01 <- crop_raster(mon_buffer, s_01)
 colnames(mon_s_01)[1] <- "s_01"
@@ -163,15 +331,19 @@ mon_s <- left_join(mon_s_01, mon_s_16) %>%
   gather(years, s_dep, c("s_01", "s_16"))
 
 
+s_years_labs <- c("2000-2002", "2015-2017")
+names(s_years_labs) <- c("s_01", "s_16") 
 
 
 # plotting
-
-ggplot() +
-  geom_tile(aes(x,y, fill = n_01), data = mon_n_01) +
+# nitrogen
+n_dep_plot <- ggplot() +
+  geom_tile(aes(x,y, fill = n_dep), color = NA, data = mon_n) +
   theme_minimal() +
-  scale_fill_viridis(name = "Total N (kg-N/ha)") + 
+  scale_fill_viridis(direction = 1, name = "Total N (kg-N/ha)", limits = c(5,18)) + 
   geom_sf(fill = NA, color = "white", data = mon_sf) +
+  facet_grid(cols = vars(years), labeller = labeller(years = n_years_labs), scales = "free") +
+  labs(title = "Total Nitrogen Deposition across Monongahela NF") +
   theme(axis.line = element_blank(),
         axis.text = element_blank(),
         axis.ticks = element_blank(),
@@ -180,7 +352,50 @@ ggplot() +
         panel.border = element_blank(),
         panel.grid = element_blank(),
         panel.spacing = unit(0, "lines"),
-        plot.background = element_blank())
+        # plot.margin = margin(0,0,0,0),
+        plot.background = element_blank(),
+        title = element_text(size = 16),
+        strip.text = element_text(size = 14, margin = margin(6, 0, -1, 0)),
+        strip.background = element_blank(),
+        # strip.placement = "outside",
+        legend.text = element_text(size = 12),
+        legend.key.height = unit(0.5, "in")) 
+
+ggsave("figures/n_dep_mon_nf.pdf", n_dep_plot, height = 8.5, width = 11)
+
+
+
+# sulfur
+s_dep_plot <- ggplot() +
+  geom_tile(aes(x,y, fill = s_dep), color = NA, data = mon_s) +
+  theme_minimal() +
+  scale_fill_viridis(direction = 1, name = "Total S (kg-N/ha)", limits = c(1,45)) + 
+  geom_sf(fill = NA, color = "white", data = mon_sf) +
+  facet_grid(cols = vars(years), labeller = labeller(years = s_years_labs)) +
+  labs(title = "Total Sulfur Deposition across Monongahela NF") +
+  theme(axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_blank(),
+        panel.grid = element_blank(),
+        panel.spacing = unit(0, "lines"),
+        # plot.margin = margin(0,0,0,0),
+        plot.background = element_blank(),
+        title = element_text(size = 16),
+        strip.text = element_text(size = 14, margin = margin(6, 0, -1, 0)),
+        strip.background = element_blank(),
+        # strip.placement = "outside",
+        legend.text = element_text(size = 12),
+        legend.key.height = unit(0.5, "in")) 
+
+ggsave("figures/s_dep_mon_nf.pdf", s_dep_plot, height = 8.5, width = 11)
+
+
+#############################################################################
+## archive
+#############################################################################
 
 
 
